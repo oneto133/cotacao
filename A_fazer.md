d82a9ea732655ea1351e468e717d201ef24e7594
Neto <rodriguesarmando225@gmail.com>
2024-12-29 18:24:55 -0300

    dei suporte para fundo imobiliário

--- A_fazer.md
+++ A_fazer.md
@@ -2,5 +2,5 @@
 
 
 *Lista de atualizações*
+
 Preciso dá um suporte para bitcoin
-precisa estar atenta as divs, pricipalmente as que acessa com índice, pois elas mudam na página html
--- main.py
+++ main.py
@@ -14,10 +14,22 @@ class conexão:
         
         cont = 0
         for c in range(0,len(lista_de_url)):
-            self.main(lista_de_url[cont])
+            self.main(lista_de_url[cont], "-1")
             cont += 1
-
-    def main(self, url):
+            
+    def main(self, url, tipo):
+        parametros = {
+            "-1": (lambda cotação, código, variações, rendimento_atual, equivalente:
+                   f"""Cotação atual {cotação} *Sem mais informações sobre a moeda"""),
+            "2": (lambda cotação, código, variações, rendimento_atual, equivalente:f"""
+A cotação atual é {cotação} o código da moeda é {código}
+no dia de hoje a moeda ficou entre {variações} o rendimento atual é {rendimento_atual}
+isso equivale a R${equivalente:.2f} de lucro."""
+),
+"3": (lambda cotação, código, variações, rendimento_atual, equivalente: 
+      f"""
+A cotação atual é {cotação} de {código} e as variações do dia foram entre {variações}""")
+}
 
         try:
             CSV = csv()
@@ -27,19 +39,24 @@ class conexão:
 
             self.resposta = requests.get(url)
             tratamento = Tratamento_de_strings()
+
             if self.resposta.status_code == 200:
                 soup = BeautifulSoup(self.resposta.text, "html.parser")
-                self.cotação = soup.select_one("div.YMlKec.fxKbKc")
+                cotação = soup.select_one("div.YMlKec.fxKbKc")
                 self.código = url[37:42]
-                self.variações = soup.select("div.P6K39c")
-                self.rendimentos = self.variações[6]
-                print("cotação atual ", tratamento.tratar_dados_de_url(self.cotação), 
-                    "Código", self.código,
-                    "variações do dia", tratamento.tratar_dados_de_url(self.variações[2]),
-                    "rendimento atual", tratamento.tratar_dados_de_url(self.rendimentos), 
-                    f"isso equivale a R${tratamento.tratar_valores_url(self.cotação, self.rendimentos):.2f} ao ano.")
-                
-                CSV.escrever_csv(conteudo=(self.código, tratamento.tratar_dados_de_url(self.cotação),
+                variação_do_preço = soup.select("div.P6K39c")
+                rendimentos = variação_do_preço[6]
+                self.cotação = tratamento.tratar_dados_de_url(cotação)
+                self.rendimento = tratamento.tratar_dados_de_url(rendimentos)
+                self.variações = tratamento.tratar_dados_de_url(variação_do_preço[2])
+                self.equivalente = tratamento.tratar_valores_url(cotação, rendimentos)
+
+                if str(tipo) in parametros:
+                    print(parametros[tipo](cotação=self.cotação, código=self.código,
+                                           variações=self.variações, rendimento_atual=self.rendimento,
+                                           equivalente=self.equivalente))
+
+                CSV.escrever_csv(conteudo=(self.código, self.cotação,
                                            hora, data),
                                  nome=r"csv/dados_das_cotações.csv", tipo="a"
                                            )
@@ -48,5 +65,10 @@ class conexão:
         except:
             print("Erro, url inválida!")
 
+    def tipo_de_ativo(self, url):
+        pontos = str(url[40:])
+        número = pontos.find(":")
+        return número
+
 if __name__ == "__main__":
-    conexão()
+    conn = conexão()
